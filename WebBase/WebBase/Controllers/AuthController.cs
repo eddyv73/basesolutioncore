b6551354dac770a113db54e7ef130a72c15a7884
wister <etorres@bside.com.mx>
2020-04-11 12:57:27 -0500

    UserManager Identity

---
+++ WebBase/WebBase/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebBase.Models;
+
+namespace WebBase.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByEmailAsync(ConfigurationManager.AppSetting["User"]);
+
+            var isLoged = await _signInManager.PasswordSignInAsync(user, ConfigurationManager.AppSetting["Pwd"], true, false);
+            return View();
+        }
+
+    }
+}
--- WebBase/WebBase/Startup.cs
+++ WebBase/WebBase/Startup.cs
@@ -12,6 +12,7 @@ using WebBase.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebBase.Models;
 
 namespace WebBase
 {
@@ -30,8 +31,9 @@ namespace WebBase
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
